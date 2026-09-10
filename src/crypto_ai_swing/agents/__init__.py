8371d1bd9e077923ce1e0210d5c834fb0b25352a
Ayoub <ayoub.alhari@hva.nl>
2026-09-10 23:59:51 +0900

    Round 42: finalize public agent exports

--- src/crypto_ai_swing/agents/__init__.py
+++ src/crypto_ai_swing/agents/__init__.py
@@ -1,5 +1,16 @@
+from .chief import ChiefAgent
+from .manager import AgentManager
 from .panel import build_research_agent_panel
+from .performance_governor import PerformanceGovernor
 from .runtime import AgentRuntime
 from .training import AgentTrainer, TrainingResult
 
-__all__ = ["build_research_agent_panel", "AgentRuntime", "AgentTrainer", "TrainingResult"]
+__all__ = [
+    "AgentManager",
+    "AgentRuntime",
+    "AgentTrainer",
+    "ChiefAgent",
+    "PerformanceGovernor",
+    "TrainingResult",
+    "build_research_agent_panel",
+]
